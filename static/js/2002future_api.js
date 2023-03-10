6e442e759a7bfeaa57495c6cf5ba6b1843a43a3a
yinqiancheng <yinqiancheng@qq.com>
2023-03-10 14:25:21 +0800

    api提取出来，方便修改

---
+++ static/js/2002future_api.js
@@ -0,0 +1,25 @@
+var  api_wuyu="https://api.yimian.xyz/img?type=head";  //头像
+var  bg_image=["https://api.yimian.xyz/img?type=head",
+               "https://api.r10086.com/img-api.php?type=P站系列1",
+			   "https://api.r10086.com/img-api.php?type=风景系列1"
+              ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("api_use1").setAttribute("src",api_wuyu);//头像
+var random=Math.floor(Math.random()*bg_image.length);//随机背景图 会根据数组长度加
+document.getElementById("nav").style.backgroundImage="url("+bg_image[random]+")";//随机背景图
