2319c2348bb91c342220d30c1230912388ff5c67
yinqiancheng <yinqiancheng@qq.com>
2023-03-17 16:12:50 +0800

    doing..

---
+++ static/js/GPT_properties.js
@@ -0,0 +1,16 @@
+// function a() {
+// 	var result1;
+// 	var data = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+// 	for (var j = 0; j < 500; j++) {
+// 		var result = "";
+// 		for (var i = 0; i < 20; i++) {
+// 			r = Math.floor(Math.random() * 16);
+// 			result += data[r];
+// 		}
+// 	}
+// 	window.localStorage.setItem("browserId", result);
+// 	localStorage.removeItem("token");
+// 	localStorage.removeItem("__DC_STAT_UUID");
+// 	  setInterval(a, 1000)
+// }
+
